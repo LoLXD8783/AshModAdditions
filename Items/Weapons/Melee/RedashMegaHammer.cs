f12dca955cf0dab770a4bd1746de26703ffa1048
LoLXD8783 <lelomlalol@gmail.com>
2020-07-08 12:38:56 -0500

    Redash mega hammer and saber

---
+++ Items/Weapons/Melee/RedashMegaHammer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using AshModAdditions.Items.Materials;
+
+namespace AshModAdditions.Items.Weapons.Melee
+{
+    public class RedashMegaHammer : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Redash MegaHammer");
+            Tooltip.SetDefault("pat was here");
+        }
+
+        public override void SetDefaults()
+        {
+            item.autoReuse = true;
+            item.melee = true;
+            item.damage = 80;
+            item.width = 122;
+            item.height = 122;
+            item.useAnimation = 30;
+            item.useTime = 30;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.crit = 13;
+            item.knockBack = 4;
+
+            item.UseSound = SoundID.Item18;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<RedashBar>(), 20);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Items/Weapons/Melee/RedashSaber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using AshModAdditions.Items.Materials;
+
+namespace AshModAdditions.Items.Weapons.Melee
+{
+    public class RedashSaber : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Redash Saber");
+            Tooltip.SetDefault("It has a blue banner on it that says, \"pats blessing\"");
+        }
+
+        public override void SetDefaults()
+        {
+            item.melee = true;
+            item.autoReuse = true;
+            item.damage = 70;
+            item.useTime = 15;
+            item.useAnimation = 15;
+            item.crit = 17;
+            item.width = 76;
+            item.height = 76;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.UseSound = SoundID.Item18;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<RedashBar>(), 19);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
