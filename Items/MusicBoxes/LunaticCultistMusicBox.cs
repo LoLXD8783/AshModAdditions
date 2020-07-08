56b3ec8b461616000a6c423f09f10e473752fe55
LoLXD8783 <lelomlalol@gmail.com>
2020-07-08 17:36:54 -0500

    Lunatic cultist music box

---
+++ Items/MusicBoxes/LunaticCultistMusicBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using AshModAdditions.Tiles.MusicBoxes;
+
+namespace AshModAdditions.Items.MusicBoxes
+{
+    public class LunaticCultistMusicBox : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lunatic Cultist Music Box");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useTime = 10;
+            item.useAnimation = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.createTile = ModContent.TileType<LunaticCultistMusicBoxTile>();
+            item.consumable = true;
+            item.accessory = true;
+        }
+    }
+}
---
+++ Tiles/MusicBoxes/LunaticCultistMusicBoxTile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using AshModAdditions.Items.MusicBoxes;
+
+namespace AshModAdditions.Tiles.MusicBoxes
+{
+    public class LunaticCultistMusicBoxTile : MusicBoxTile
+    {
+        public override string MusicBoxName => "Lunatic Cultist Music Box";
+        public override int ItemType => ModContent.ItemType<LunaticCultistMusicBox>();
+    }
+}
