baf376b53a4aae5e352da5bf6823060746b4fcce
Pyro744 <68665017+Pyro744@users.noreply.github.com>
2020-07-23 19:52:04 +0200

    Add files via upload

---
+++ HolyWings.cs
@@ -0,0 +1,65 @@
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+
+namespace AshModAdditions.Items.Equippables
+{
+    [AutoloadEquip(EquipType.Wings)]
+    public class HolyWings : ModItem
+    {
+        //public override bool Autoload(ref string name)
+        //{
+        //    return !GetInstance<ExampleConfigServer>().DisableExampleWings;
+        //}
+
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Tell Pyro what to put in here.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 30;
+            item.value = 10000;
+            item.rare = ItemRarityID.Green;
+            item.accessory = true;
+        }
+        //these wings use the same values as the solar wings
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.wingTimeMax = 140; // might be 140 tiles
+        }
+
+        public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
+            ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
+        {
+            ascentWhenFalling = 0.85f;
+            ascentWhenRising = 0.15f;
+            maxCanAscendMultiplier = 1f;
+            maxAscentMultiplier = 3f;
+            constantAscend = 0.135f;
+        }
+
+        public override void HorizontalWingSpeeds(Player player, ref float speed, ref float acceleration)
+        {
+            speed = 67f;
+            acceleration *= 2.5f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+           /* recipe.AddIngredient(mod, "HolyFeather"); Enable when the Holy Feather was added it */
+            recipe.AddIngredient(ItemID.SoulofLight, 20);
+            recipe.AddIngredient(ItemID.HallowedBar, 10);
+            recipe.AddIngredient(ItemID.SoulofFright);
+            recipe.AddIngredient(ItemID.SoulofMight);
+            recipe.AddIngredient(ItemID.SoulofSight);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
