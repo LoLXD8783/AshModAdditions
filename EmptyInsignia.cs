ff466c6c08cbbd97a17e4e263c751a49b7873bf9
Pyro744 <68665017+Pyro744@users.noreply.github.com>
2020-07-26 20:55:24 +0200

    Add files via upload
    nerfed Holy wings

---
+++ EmptyInsignia.cs
@@ -0,0 +1,33 @@
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AshModAdditions.Items.Materials
+{
+    public class EmptyInsignia : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Empty Insignia");
+            Tooltip.SetDefault("Tell Pyro what to put in here");
+        }
+
+        public override void SetDefaults()
+        {
+            item.material = true;
+            item.width = 30;
+            item.height = 28;
+            item.maxStack = 99;
+            item.value = Item.sellPrice(gold: 1);
+            item.rare = 9;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(3467, 10);
+            recipe.AddTile(134);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ GreenSwipe.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AshModAdditions.Items.Weapons.Melee
+{
+    public class GreenSwipe : ModItem // TODO: Missing projectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Green Swipe");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 90;
+            item.melee = true;
+            item.width = 42;
+            item.height = 42;
+            item.useTime = 7;
+            item.useAnimation = 25;
+            item.channel = true;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.useStyle = 5;
+            item.knockBack = 6;
+            item.value = Item.buyPrice(0, 22, 50, 0);
+            item.rare = 9;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.shoot = 595;
+            item.shootSpeed = 40f;
+
+        }
+    }
+}
